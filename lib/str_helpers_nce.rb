a4af836e31b19970414abde27e791698f6a462e7
Nate <nathanestes@gmail.com>
2018-10-15 10:51:43 -0600

    works

--- lib/str_helpers_nce.rb
+++ lib/str_helpers_nce.rb
@@ -1,5 +1,26 @@
 require "str_helpers_nce/version"
 
 module StrHelpersNce
-  # Your code goes here...
+
+  def self.reversify(str)
+    str.split('').reverse.join('')
+  end
+
+  def self.casify(str)
+    to_case = [:upcase, :downcase]
+    arr = str.split('')
+    arr.each_with_index do |letter, i|
+      this_case = to_case.sample
+      arr[i] = letter.send(this_case)
+    end
+    arr.join('')
+  end
+
+  def self.spacify(str, spaces = 0)
+    spaces.times do 
+      str = str.split('').join(' ')
+    end
+    str
+  end
+
 end
